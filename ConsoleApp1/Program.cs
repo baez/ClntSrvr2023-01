5e4d8a42704dce60fd402c7a1757913120747f2a
GB Karim <george.bekari@gmail.com>
2023-01-17 23:13:03 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+    using ConsoleApp1.Network;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.TestHostInformation();
+        }
+
+        private void TestHostInformation()
+        {
+            var hostInfo = new HostInformation().GetHostIPAddresses("www.ibm.com");
+        }
+    }
+}
