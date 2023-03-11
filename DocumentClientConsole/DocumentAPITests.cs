fc6edc6ee5a4289f565ed42cb69d59a2e5eae62f
GB Karim <behzadk0@gmail.com>
2023-03-11 09:55:38 -0800

    Renamed the projects. Added a client console App.

--- DocumentClientConsole/DocumentAPITests.cs
+++ DocumentClientConsole/DocumentAPITests.cs
@@ -4,14 +4,22 @@
     using System.Net.Http;
     using System.Threading.Tasks;
 
-    internal class DocumentClientProgram
+    internal class DocumentAPITests
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello from Document Client Console App!");
+            var testApp = new DocumentAPITests();
+            await testApp.GetDocumentTest();   
+        }
 
-            var documentClientProgram = new DocumentClientProgram();
-            var baseUri = "https://docservgbk20230224.azurewebsites.net/api/document"; // localhost:44385 https://docservgbk20230224.azurewebsites.net/api/document
+        private async Task GetDocumentTest()
+        {
+            Console.WriteLine("Get Document Test");
+
+            var documentClientProgram = new DocumentAPITests();
+            // https://localhost:44385/api/document?id=ha12 -- https://localhost:44385/api/document?id={id}";
+            // localhost:44385 https://docservgbk20230224.azurewebsites.net/api/document
+            var baseUri = "https://docservgbk20230224.azurewebsites.net/api/document";
             var id = "3";
             var doc = await documentClientProgram.GetDocument(baseUri, id);
 
@@ -25,7 +33,6 @@
             {
                 var httpClient = new HttpClient();
 
-                // https://localhost:44385/api/document?id=ha12 -- https://localhost:44385/api/document?id={id}";
                 var uri = $"{baseUri}?id={id}";
                 documentAsString = await httpClient.GetStringAsync(uri);
             }
